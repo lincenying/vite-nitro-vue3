fb8923e4f027c7f5b3558252a6daa3c39880958c
LinCenYing <lincenying@qq.com>
2025-07-01 17:35:36 +0800

    chore: 更新一些东西

--- src/components/global/header.vue
+++ src/components/global/header.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="global-header" flex="~ none justify-center items-center" fixed z-999 left-0 top-0 w-100vw h="80px!" bg-hex-fff>
-        <div flex="~ auto" max-w-1293px>
+        <div flex="~ auto" max-w-1294px>
             <img :src="NavLogo" w-109px h-31px alt="">
             <div flex="~ items-center" ml-64px>
                 <el-dropdown mr-32px @command="handleCommand">
@@ -21,6 +21,7 @@
                 <router-link mr-32px text-16px lh-32px to="/news">新闻中心</router-link>
                 <router-link mr-32px text-16px lh-32px to="/faqs">常见问题</router-link>
                 <router-link mr-32px text-16px lh-32px to="/article">SQLite文章</router-link>
+                <router-link mr-32px text-16px lh-32px to="/pdf">PDF</router-link>
             </div>
         </div>
     </div>
